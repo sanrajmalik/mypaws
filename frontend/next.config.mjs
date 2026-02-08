6eb0dc40faddf82587af7b0455c5853330eef1ac
Sanraj Malik <68771201+sanrajmalik@users.noreply.github.com>
2026-02-09 02:46:32 +0530

    mising conf

---
+++ frontend/next.config.mjs
@@ -0,0 +1,38 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "standalone",
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '5000',
+                pathname: '/uploads/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'api',
+                port: '5000',
+                pathname: '/uploads/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            }
+        ],
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/uploads/:path*',
+                destination: `${(process.env.INTERNAL_API_URL || 'http://localhost:5000').replace(/\/api\/?$/, '')}/uploads/:path*`,
+            },
+            {
+                source: '/api/:path*',
+                destination: `${process.env.INTERNAL_API_URL || 'http://localhost:5000/api'}/:path*`,
+            }
+        ];
+    },
+};
+
+export default nextConfig;
